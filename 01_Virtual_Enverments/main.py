caf09981e6e5236e047f9455cf80d82562a30c2d
Subhasish-MoveCore <subhasishsahoo1357@gmail.com>
2026-05-29 19:18:36 +0530

    virtual enverment

---
+++ 01_Virtual_Enverments/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI 
+app = FastAPI ()
+@app.get("/")
+def hello():
+    return{'message':'Hello World'}
+
+@app.get("/about")
+def about():
+    return {'message': 'i love this FastAPI'} 
